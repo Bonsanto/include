aaf42b2642126aee5af89d1a864c31477514f13f
Bonsanto <xbonsx@gmail.com>
2015-02-24 21:18:33 -0430

    Comments are deleted.

--- experimental/Import.js
+++ experimental/Import.js
@@ -1,11 +1,6 @@
 var apply = eval;
 
 var include = function (path) {
-	//var script = document.createElement("script");
-	//script.type = "text/javascript";
-	//script.src = path;
-	//
-	//document.head.appendChild(script);
 	var xml = new XMLHttpRequest();
 
 	xml.open("GET", path, false);
